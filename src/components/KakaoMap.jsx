04eaca29aff3727bf32fb60faa92c20f60af40fc
sikkkkkw <gksmsla92@naver.com>
2024-09-05 11:49:09 +0900

    번호넣음

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -69,22 +69,22 @@ export default function KakaoMap() {
     return (
         <div className="flex flex-col justify-center items-center py-20 md:py-24 px-4 bg-gray-100">
             <style>{`
-    html, body {width:100%;height:100%;margin:0;padding:0;} 
-    .map_wrap {position:relative;overflow:hidden;width:100%;height:350px;}
-    .radius_border{border:1px solid #919191;border-radius:5px;}     
-    .custom_typecontrol {position:absolute;top:10px;right:10px;overflow:hidden;width:130px;height:30px;margin:0;padding:0;z-index:1;font-size:12px;font-family:'Malgun Gothic', '맑은 고딕', sans-serif;}
-    .custom_typecontrol span {display:block;width:65px;height:30px;float:left;text-align:center;line-height:30px;cursor:pointer;}
-    .custom_typecontrol .btn {background:#fff;background:linear-gradient(#fff,  #e6e6e6);}       
-    .custom_typecontrol .btn:hover {background:#f5f5f5;background:linear-gradient(#f5f5f5,#e3e3e3);}
-    .custom_typecontrol .btn:active {background:#e6e6e6;background:linear-gradient(#e6e6e6, #fff);}    
-    .custom_typecontrol .selected_btn {color:#fff;background:#425470;background:linear-gradient(#425470, #5b6d8a);}
-    .custom_typecontrol .selected_btn:hover {color:#fff;}   
+                html, body {width:100%;height:100%;margin:0;padding:0;} 
+                .map_wrap {position:relative;overflow:hidden;width:100%;height:350px;}
+                .radius_border{border:1px solid #919191;border-radius:5px;}     
+                .custom_typecontrol {position:absolute;top:10px;right:10px;overflow:hidden;width:130px;height:30px;margin:0;padding:0;z-index:1;font-size:12px;font-family:'Malgun Gothic', '맑은 고딕', sans-serif;}
+                .custom_typecontrol span {display:block;width:65px;height:30px;float:left;text-align:center;line-height:30px;cursor:pointer;}
+                .custom_typecontrol .btn {background:#fff;background:linear-gradient(#fff,  #e6e6e6);}       
+                .custom_typecontrol .btn:hover {background:#f5f5f5;background:linear-gradient(#f5f5f5,#e3e3e3);}
+                .custom_typecontrol .btn:active {background:#e6e6e6;background:linear-gradient(#e6e6e6, #fff);}    
+                .custom_typecontrol .selected_btn {color:#fff;background:#425470;background:linear-gradient(#425470, #5b6d8a);}
+                .custom_typecontrol .selected_btn:hover {color:#fff;}   
 
-    .custom_zoomcontrol {position:absolute;top:50px;right:10px;width:36px;height:80px;overflow:hidden;z-index:1;background-color:#f5f5f5;} 
-    .custom_zoomcontrol span {display:flex;align-items:center;justify-content:center;width:36px;height:40px;cursor:pointer;}     
-    .custom_zoomcontrol span img {width:20px;height:20px;border:none;}             
-    .custom_zoomcontrol span:first-child {border-bottom:1px solid #bfbfbf;} 
-`}</style>
+                .custom_zoomcontrol {position:absolute;top:50px;right:10px;width:36px;height:80px;overflow:hidden;z-index:1;background-color:#f5f5f5;} 
+                .custom_zoomcontrol span {display:flex;align-items:center;justify-content:center;width:36px;height:40px;cursor:pointer;}     
+                .custom_zoomcontrol span img {width:20px;height:20px;border:none;}             
+                .custom_zoomcontrol span:first-child {border-bottom:1px solid #bfbfbf;} 
+            `}</style>
 
 
             <h2 className="text-center text-3xl lg:text-4xl font-bold mb-6 lg:mb-8">오시는 길</h2>
@@ -113,6 +113,7 @@ export default function KakaoMap() {
                         <p><strong>도로명: 대구 수성구 청솔로2길 37 1층</strong></p>
                         <p><strong>지번: 황금동 691-3</strong></p>
                         <p><strong>우편번호: 42144</strong></p>
+                        <p><strong>전화번호: 053-768-7738</strong></p>
 
                         <h2 className="text-2xl font-semibold text-gray-800 mt-6">영업시간</h2>
                         <p><strong>화~수: 11:00 ~ 20:00</strong></p>
