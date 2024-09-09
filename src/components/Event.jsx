db281e4015698d0559e9fafcfabe70956e7fe8d3
sikkkkkw <gksmsla92@naver.com>
2024-09-09 13:27:40 +0900

    텍스트수정

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -15,7 +15,7 @@ export default function Event({ isVisible, handleClose }) {
     isVisible && (
       <div className='bg-blue-400 w-full h-8 z-50 fixed top-0 left-0 flex justify-between items-center fonttest'>
         <div className='flex-1 text-center text-sm md:text-base lg:text-lg truncate'>
-          안녕하세요. 마음입니다! 한분 한분 최선을 다해 만족 시켜 드리겠습니다.
+          마음입니다! 한분 한분 최선을 다해 만족 시켜 드리겠습니다.
         </div>
         <div className='pr-4 cursor-pointer' onClick={handleClose}>
           <img src={X} alt="Close" className="h-4 w-4" /> 
